349bc367e7685a3165591c425b2bee80dffbb044
Joseph  Borne <bourne_joseph_pierre@student.smc.edu>
2022-10-13 02:23:19 +0000

    small changes made. i need to re-watch lecture after he uploads the recording.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+
 const app = express();
 
 // We only use express to serve the main client ( index.html )
@@ -10,12 +11,20 @@ app.use(express.static("public"));
 const http = app.listen(3000);
 
 
-
 // Code below is socket.io related
 
 // socket.io needs a http instance
-const io = socketio(http);
+const io = socketio(http); // this automatically creates a static script resource at location /socket.io/socket.io.js
 
 io.on("connection", function(client) {
+  console.log("A client has connected");
 
+  client.on("newChat", function(chatMessage) {
+    io.emit("chat")
+    console.log(chatMessage);
+  })
+
+  client.on("disconnect", function() {
+    console.log("A client has disconnected");
+  })
 })
